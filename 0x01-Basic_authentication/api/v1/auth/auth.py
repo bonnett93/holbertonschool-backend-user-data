6ba1cbf63f6f51a139adfdfdc3661fb86409cc79
bonnett93 <bonnettjordan@gmail.com>
2022-05-27 23:17:39 -0500

    Upload task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,19 @@ class Auth():
     """class to manage the API authentication."""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns False - path and excluded_paths"""
-        return False
+        """returns True if the path is not in the list
+        of strings excluded_paths
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
