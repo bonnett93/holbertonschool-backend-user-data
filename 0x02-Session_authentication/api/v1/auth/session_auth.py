d7d8494c34c64adb0fe687703077b062228df35d
bonnett93 <bonnettjordan@gmail.com>
2022-05-30 22:46:19 -0500

    Create create_session method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """session_auth"""
+from telnetlib import SE
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session Auth Class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) != str:
+            return None
+        SESSION_ID = str(uuid.uuid4())
+        self.user_id_by_session_id[SESSION_ID] = user_id
+        return SESSION_ID
